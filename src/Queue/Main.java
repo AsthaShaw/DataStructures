7ee645a7c2bfcb9579fe49589c923047be74a698
asthashaw <astha.shaw100@gmail.com>
2024-07-05 06:55:01 +0100

    Add linkedlist implementation of queue

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -3,6 +3,15 @@ package Queue;
 public class Main {
 
     public static void main(String[] args) {
+       Queue myQueue=new Queue(1);
+       myQueue.enqueue(2);
+       myQueue.getFirst();
+       myQueue.getLast();
+       myQueue.getLength();
 
+       myQueue.printQueue();
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue().value);
+        System.out.println(myQueue.dequeue());
     }
 }
